ae3f0b504bdb38e205c3f26e07f354336dd2ab2c
kawada <vgnsl94@gmail.com>
2020-04-28 07:09:18 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,6 @@
    <div class="jumbotron text-center">
         <h1 class="display-4">comics</h1>
         <p class="lead">気になる漫画を整理</p>
-        <a class="btn btn-success btn-lg" href="#" role="button">登録</a>
-      </div>
+        {!! link_to_route('signup.get', '登録!', [], ['btn btn-success btn-lg']) !!}
+   </div>
 @endsection
